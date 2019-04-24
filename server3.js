074916ff3e275c7de3e2799fa884608557d25d04
tettus23m <s1732089BN@s.chibakoudai.jp>
2019-04-24 07:18:04 +0000

    sumple

--- server3.js
+++ server3.js
@@ -2,14 +2,17 @@ var http = require('http');
 var server =http.createServer();
 
 server.on( 'request', function(req,res) {
-    var now = new Date().getTime();
+    var t = new Date();
+    var h = t.getHours();
+    var m = t.getMinutes();
+    var s = t.getSeconds();
     res.writeHead( 200, {'Content-Type' : 'text/html' });
     res.write('<!DOCTYPE html>');
     res.write('<html lang=ja>');
     res.write('<head><meta charset="UTF-8"></head>');
     res.write('<body>');
     res.write('<h1>Hello world</h1>');
-    res.write('現在の時刻は'+now);
+    res.write('現在の時刻は'+h+'時'+m+'分'+s+'秒');
     res.write('</body>');
     res.write('</html>');
     res.end();
